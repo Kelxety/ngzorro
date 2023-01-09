ee5948c0554c708c00fe8c89987fee032a12c3ea
Kelxety <llanzanarobinjohn@gmail.com>
2023-01-09 23:16:26 +0800

    First commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { UntypedFormGroup, UntypedFormBuilder, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+  
+export class LoginComponent {
+  validateForm!: UntypedFormGroup;
+
+  submitForm(): void {
+    console.log('submit', this.validateForm.value);
+  }
+
+  constructor(private fb: UntypedFormBuilder) {}
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      userName: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+      remember: [true]
+    });
+  }
+}
